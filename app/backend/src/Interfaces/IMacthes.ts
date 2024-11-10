71c8fd007c93a85eb8beac653abf6c006908e13c
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-11-10 13:23:12 -0300

    fix lint

--- app/backend/src/Interfaces/IMacthes.ts
+++ app/backend/src/Interfaces/IMacthes.ts
@@ -1,9 +1,8 @@
 export default interface Matches {
-    id: number,
-    homeTeamId: number,
-    homeTeamGoals: number,
-    awayTeamId: number,
-    awayTeamGoals: number,
-    inProgress: boolean,
-  }
-  
+  id: number,
+  homeTeamId: number,
+  homeTeamGoals: number,
+  awayTeamId: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
+}
